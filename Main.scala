8e746a5e9e14c15822e8f232a83691dda2d4e295
no <leachmanh@gmail.com>
2022-10-05 04:06:57 -0500

    remove old main

--- Main.scala
+++
@@ -1,8 +0,0 @@
-import zio.http._
-
-val app = Http.collectZIO[Request] {
-    case req @ Method.GET -> !! / "fruits" / "a"  =>
-      UIO(Response.text("URL:" + req.url.path.asString + " Headers: " + req.getHeaders))
-    case req @ Method.POST -> !! / "fruits" / "a" =>
-      req.bodyAsString.map(Response.text(_))
-  }
